4a8013caabe4de621b8c44a9be2a47e6aafa4bfb
aleanrocha <aleanrocha.dev@gmail.com>
2024-07-22 07:52:22 -0300

    primeiro desafio

---
+++ desafios/01-primeiro-desafio/programa.js
@@ -0,0 +1,11 @@
+// PRIMEIRO DESAFIO
+
+// 01 - Crie um noo projeto NodeJS
+// 02 - Crie um arquivo com o nome programa
+// 02 - Crie 2 variáveis e imprima a soma entre elas
+// 04 - Execute o arquivo e veja a resposta no terminal
+
+const n1 = 10
+const n2 = 15
+
+console.log(`A soma ne ${n1} + ${n2} = ${n1+n2}.`)
