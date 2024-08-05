7f6ee44119f139951b991d93ba47127e0e19844e
aleanrocha <aleanrocha.dev@gmail.com>
2024-08-05 08:15:16 -0300

    criando layouts

---
+++ 06-template-engine/02-criando-layouts/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+const port = 3001
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.listen(port, () => console.log('Server started on port %s', port))
