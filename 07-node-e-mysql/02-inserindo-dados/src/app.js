7cda9721c6f216ea20e4a32cf076b08ce30120d7
aleanrocha <aleanrocha.dev@gmail.com>
2024-08-10 12:53:58 -0300

    inserindo dados no banco

---
+++ 07-node-e-mysql/02-inserindo-dados/src/app.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const mysql = require('mysql2')
+
+const app = express()
+const port = 3001
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+app.set('views', './src/views')
+
+app.use(express.static('public'))
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  return res.render('home')
+})
+
+app.post('/books/insertbook', (req, res) => {
+  const title = req.body.title
+  const pageQtn = req.body.pageqty
+
+  const query = `INSERT INTO books (title, pageqty) VALUES ('${title}', '${pageQtn}')`
+
+  conn.query(query, (err) => {
+    if (err) {
+      console.log(err)
+    }
+    res.redirect('/')
+  })
+})
+
+const conn = mysql.createConnection({
+  host: 'localhost',
+  port: '3306',
+  user: 'root',
+  password: 'nodemysql',
+  database: 'booksdb'
+})
+
+conn.connect((err) => {
+  console.log('Connecting...')
+  if (err) {
+    console.log('Ops, deu ruim!', err)
+    return
+  }
+  console.log('Successfully conected.')
+  app.listen(port, () => console.log('Server started on por %s', port))
+})
