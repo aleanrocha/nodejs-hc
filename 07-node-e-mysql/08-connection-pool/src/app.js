bae2a6f159b9e3fd3c93f388b9f1c26ecec3ae4a
aleanrocha <aleanrocha.dev@gmail.com>
2024-08-11 16:50:32 -0300

    connection pool

---
+++ 07-node-e-mysql/08-connection-pool/src/app.js
@@ -0,0 +1,104 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const pool = require('./db/conn.js')
+
+const app = express()
+const port = 3001
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+app.set('views', './src/views')
+
+app.use(express.static('public'))
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  return res.render('home')
+})
+
+// salva os dados no banco
+app.post('/books/insertbook', (req, res) => {
+  const title = req.body.title
+  const pageQtn = req.body.pageqty
+  const query = `INSERT INTO books (title, pageqty) VALUES ('${title}', '${pageQtn}')`
+  pool.query(query, (err) => {
+    if (err) {
+      console.log(err)
+    }
+    return res.redirect('/books')
+  })
+})
+
+// pegar os dados do banco
+app.get('/books', (req, res) => {
+  // pega o dado da barra de pesquisa
+  const search = req.query.search
+  let query = `SELECT * FROM books`
+  // verifica se o dado existe e caso exista faz a busca individual
+  if (search) {
+    query = `SELECT * FROM books WHERE title = '${search}'`
+  }
+  pool.query(query, (err, data) => {
+    if (err) {
+      console.log(err)
+      return
+    }
+    return res.render('books', { data, search })
+  })
+})
+
+// pegar os dados de forma individual
+app.get('/book/:id', (req, res) => {
+  const id = req.params.id
+  const query = `SELECT * FROM books WHERE id = '${id}'`
+  pool.query(query, (err, data) => {
+    if (err) {
+      console.log(err)
+      return
+    }
+    const book = data[0]
+    return res.render('book', { book })
+  })
+})
+
+// obter livro pra fazer a edição
+app.get('/book/edit/:id', (req, res) => {
+  const id = req.params.id
+  const query = `SELECT * FROM books WHERE id = '${id}'`
+  pool.query(query, (err, data) => {
+    if (err) {
+      console.log(err)
+      return
+    }
+    const book = data[0]
+    return res.render('editBook', { book })
+  })
+})
+
+// fazer a edição do livro
+app.post('/book/updatebook', (req, res) => {
+  const { id, title, pageqty} = req.body
+  const query = `UPDATE books SET title = '${title}', pageqty = '${pageqty}' WHERE id = ${id}`
+  pool.query(query, (err) => {
+    if (err) {
+      console.log(err)
+      return
+    }
+    return res.redirect(`/book/${id}`)
+  })
+})
+
+app.post('/book/deletebook', (req, res) => {
+  const query = `DELETE FROM books WHERE id = ${req.body.id}`
+  pool.query(query, (err) => {
+    if (err) {
+      console.log(err)
+      return
+    }
+    return res.redirect('/books')
+  })
+})
+
+app.listen(port, () => console.log('Server started on por %s', port))
