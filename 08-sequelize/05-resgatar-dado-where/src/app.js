a408d13e029503740cb73d593693ae91178c7a27
aleanrocha <aleanrocha.dev@gmail.com>
2024-08-14 12:47:50 -0300

    resgatar dados pelo id

---
+++ 08-sequelize/05-resgatar-dado-where/src/app.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const conn = require('./db/conn')
+
+const User = require('./models/User')
+
+const app = express()
+const port = 3001
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+app.set('views', './src/views')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use(express.static('public'))
+
+// renderizar home
+app.get('/', (req, res) => {
+  return res.render('home')
+})
+
+// renderizar tela de adicionar novo usuário
+app.get('/adduser', (req, res) => {
+  return res.render('addUser')
+})
+
+// criar novo usuário
+app.post('/adduser/create', async (req, res) => {
+  const name = req.body.name
+  const occupation = req.body.occupation
+  let newsletter = req.body.newsletter
+  if (newsletter === 'on') {
+    newsletter = true
+  } else {
+    newsletter = false
+  }
+  await User.create({ name, occupation, newsletter })
+  return res.redirect('/users')
+})
+
+// obter todos os usuários
+app.get('/users', async (req, res) => {
+  const users = await User.findAll({ raw: true})
+  return res.render('users', { users })
+})
+
+// obter usuário pelo id
+app.get('/user/:id', async (req, res) => {
+  const id = req.params.id
+  const user = await User.findOne({ raw: true, where: { id: id } })
+  return res.render('userView', { user })
+})
+
+conn
+  .sync()
+  .then(() => {
+    app.listen(port, () => console.log('Server started on port %s', port))
+  })
+  .catch((err) => console.log(err))
