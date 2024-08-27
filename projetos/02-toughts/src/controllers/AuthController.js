9a540123a51c7f27e0e19661982a1f7904fdadc0
aleanrocha <aleanrocha.dev@gmail.com>
2024-08-27 07:14:22 -0300

    flash messages

--- projetos/02-toughts/src/controllers/AuthController.js
+++ projetos/02-toughts/src/controllers/AuthController.js
@@ -1,8 +1,28 @@
+const User = require('../models/User')
+const bcript = require('bcryptjs')
+
 module.exports = class AuthController {
   static async login(req, res) {
     return res.render('auth/login')
   }
   static async register(req, res) {
     return res.render('auth/register')
+  }
+  static async registerPost(req, res) {
+    const { name, email, password, confirmPassword } = req.body
+    if (
+      name.trim() === '' ||
+      email.trim() === '' ||
+      password.trim() === '' ||
+      confirmPassword === ''
+    ) {
+      req.flash('message', 'Por favor, preencha todos os campos!')
+      return res.render('auth/register')
+    }
+    if (confirmPassword !== password) {
+      req.flash('message', 'As senhas são diferentes')
+      return res.render('auth/register')
+    }
+
   }
 }
